c95a1a853905c438fed5e7dc45f4f4b0c80b48e1
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-03-31 22:32:25 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsAndNavApp.swift
+//  BuildingListsAndNav
+//
+//  Created by Tabita Sadiq on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
